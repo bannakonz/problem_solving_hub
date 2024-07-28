28b0b18540f95eca95359e8dd31c371d663fb2d6
bannakonz <bannakon10@gmail.com>
2024-07-28 09:03:58 +0700

    palindrome number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int input;
+        int revert = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        input = sc.nextInt();
+        int num = input;
+        while (input > 0) {
+            int digit = input % 10;
+            revert = revert * 10 + digit;
+            input = input / 10;
+        }
+
+        if (revert == num) {
+            System.out.println(num + " IS PALINDROME NUMBER");;
+        } else {
+            System.out.println(num + " IS NOT PALINDROME NUMBER");
+        }
     }
 }
