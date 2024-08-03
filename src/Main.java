85f456465a1b3994e09a39c1a701a39f3a6e9731
bannakonz <bannakon10@gmail.com>
2024-08-03 15:52:53 +0700

    convert fahrenheit to celsius

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    static double calculateFuelConsumption(double length, double litter) {
-        return  length/litter;
+    static double fahrenheitToCelsius(double fahrenheit) {
+        return  (fahrenheit - 32 ) * 5.0/9.0;
     }
 
+//  °F − 32) × 5/9 = °C
+
     public static void main(String[] args) {
-
-        /*
-        1. เจ้าของรถคันหนึ่งสังเกตว่าเขาขับรถของเขาไปได้ไกล 300 กิโลเมตร โดยใช้น้ำมัน 20 ลิตร เขาอยากทราบรถของเขากินน้ำมันกี่กิโลลิตร
-        ให้เติมคำสั่งที่มีการคำนวณลงไปในช่องว่างของ โปรแกรม calculateFuelConsumption เพื่อคำนวณอัตราการใช้น้ำมันรถคันนั้น
-        */
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the distance traveled (in kilometers): ");
-        double distance = sc.nextDouble();
-
-        System.out.print("Enter the amount of fuel used (in liters): ");
-        double fuel = sc.nextDouble();
-
-        double kmPerLiter = calculateFuelConsumption(distance, fuel);
-        System.out.println("The fuel consumption rate of your car is: " + kmPerLiter + " kilometers per liter");
-
+        System.out.print("Enter Fahrenheit : ");
+        double fahrenheit = sc.nextDouble();
+        double celsius = fahrenheitToCelsius(fahrenheit);
+//        System.out.println(fahrenheit + "°F = " + celsius + "°C");
+        System.out.printf("%.2f°F = %.2f°C%n", fahrenheit, celsius);
     }
 
 }
