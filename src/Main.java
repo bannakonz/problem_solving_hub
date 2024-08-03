5b2fb7050cba55ffde2a75ee146d54e66f90e218
bannakonz <bannakon10@gmail.com>
2024-08-03 21:38:34 +0700

    calculate sum of first natural number

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    static double fahrenheitToCelsius(double fahrenheit) {
-        return  (fahrenheit - 32 ) * 5.0/9.0;
+
+    static int sumOfFirstNaturalNumber(int n) {
+//        int sum = n;
+//        for (int i = n - 1; i >= 1; i--) {
+//            sum += i;
+//        }
+//        return sum;
+        return n*(n+1)/2;
     }
 
-//  °F − 32) × 5/9 = °C
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Fahrenheit : ");
-        double fahrenheit = sc.nextDouble();
-        double celsius = fahrenheitToCelsius(fahrenheit);
-//        System.out.println(fahrenheit + "°F = " + celsius + "°C");
-        System.out.printf("%.2f°F = %.2f°C%n", fahrenheit, celsius);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        int sum = sumOfFirstNaturalNumber(n);
+        System.out.println("sum = " + sum);
+
     }
 
 }
