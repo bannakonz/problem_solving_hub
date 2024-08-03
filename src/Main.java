f7b55a8ed9a0823e30d309c1b7b2f8f4a6811122
bannakonz <bannakon10@gmail.com>
2024-08-03 12:08:36 +0700

    hello world (graphic)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int input;
-        int revert = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Number : ");
-        input = sc.nextInt();
-        int num = input;
-        while (input > 0) {
-            int digit = input % 10;
-            revert = revert * 10 + digit;
-            input = input / 10;
-        }
-
-        if (revert == num) {
-            System.out.println(num + " IS PALINDROME NUMBER");;
-        } else {
-            System.out.println(num + " IS NOT PALINDROME NUMBER");
-        }
+        JFrame frame = new JFrame() {
+            public void paint(Graphics g) {
+                g.drawString("Hello, World!", 110, 150);
+                g.drawOval(100,130,90,30);
+            }
+        };
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
