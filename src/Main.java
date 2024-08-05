ded01ff1cede06c8f2e4d1e368b1df193db4823f
bannakonz <bannakon10@gmail.com>
2024-08-05 22:44:19 +0700

    area of circle

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    static double exponentCalculate(double base, double exponent) {
-        double result = 1;
-
-        if (exponent < 0) {
-            base = 1 / base;
-            exponent = -exponent;
-        }
-
-        while (exponent > 0) {
-            result*=base;
-            exponent--;
-        }
-        return result;
-    }
-
     public static void main(String[] args){
+        final double PI = 3.41;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Base Number : ");
-        double base = sc.nextDouble();
-        System.out.print("Enter Exponent Number : ");
-        double exp = sc.nextDouble();
-        double result = exponentCalculate(base,exp);
-        System.out.printf("Result = %.3f\n", result);
+        System.out.print("Enter radius : ");
+        double r = sc.nextDouble();
+        double areaOfCircle = PI * (r * r);
+        System.out.println("Area of Circle = " + areaOfCircle);
     }
 
 }
