9e865d491e0650fbac311c8739e8431e752fdab9
bannakonz <bannakon10@gmail.com>
2024-08-08 16:15:21 +0700

    time convertor, time to minute

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args){
-        final double PI = 3.41;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter radius : ");
-        double r = sc.nextDouble();
-        double areaOfCircle = PI * (r * r);
-        System.out.println("Area of Circle = " + areaOfCircle);
+    private static int toMinutes(int hour, int minute) {
+        return hour * 60 + minute;
     }
 
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter hour : ");
+        int hour = sc.nextInt();
+        System.out.print("Enter minute : ");
+        int minute = sc.nextInt();
+        int totalMinutes = toMinutes(hour,minute);
+        System.out.println(hour + " hour(s) " +  minute  + " minute(s) = " + totalMinutes + " minute(s)");
+    }
 }
