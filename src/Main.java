5c47a0642e8e5b88603a1d927f83beeddd81a1a3
bannakonz <bannakon10@gmail.com>
2025-02-14 21:23:53 +0700

    multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 public class Main {
-    private static int toMinutes(int hour, int minute) {
-        return hour * 60 + minute;
-    }
 
-    public static void main(String[] args){
+        public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        int base = scanner.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter hour : ");
-        int hour = sc.nextInt();
-        System.out.print("Enter minute : ");
-        int minute = sc.nextInt();
-        int totalMinutes = toMinutes(hour,minute);
-        System.out.println(hour + " hour(s) " +  minute  + " minute(s) = " + totalMinutes + " minute(s)");
-    }
+        System.out.println("\nMultiplication Table for " + base + ":");
+        for (int i = 1; i <= 12; i++) {
+            System.out.printf("%d * %d = %d\n", base, i, base * i);
+        }
+
+   }
+
 }
