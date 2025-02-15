de344d5cd3be897b763f577e3df6ce91fb4de5f6
bannakonz <bannakon10@gmail.com>
2025-02-15 19:03:32 +0700

    start pattern 1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,48 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter hours worked : ");
-        int hoursWorked = sc.nextInt();
-        int totalSalary = calculateSalary(hoursWorked);
-        System.out.printf("Total salary = %d", totalSalary);
-        sc.close();
-    }
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
 
-    /**
-     * คำนวณค่าจ้างรายสัปดาห์ตามเงื่อนไข:
-     * - ชั่วโมงปกติ (<= 40): 100 บาท/ชั่วโมง
-     * - ชั่วโมงล่วงเวลา (> 40): 150 บาท/ชั่วโมง
-     *
-     * @param hours จำนวนชั่วโมงที่ทำงาน
-     * @return จำนวนเงินค่าจ้างทั้งหมด
-     */
-    private static int calculateSalary(int hours) {
-        return (hours <= 40)
-                ? hours * 100
-                : (40 * 100) + ((hours - 40) * 150);
+
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//
+//                 if (i == n - 1 || j == 0 || i == j || i > j) {
+//                     System.out.print("X");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+
+
+ /*
+ Method 3
+ ex.
+ System.out.println("A".repeat(5));  // ผลลัพธ์: AAAAA
+ System.out.println("Hi".repeat(3)); // ผลลัพธ์: HiHiHi
+
+ */
+//         for (int i = 1; i <= n; i++) {
+//             System.out.println("X".repeat(i));
+//         }
+
+
+        sc.close();
     }
 }
