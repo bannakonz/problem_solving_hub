60bf9bcc74069fc5690c1ac2ce75971c5eb4b9b8
bannakonz <bannakon10@gmail.com>
2025-02-16 17:03:55 +0700

    triangle pettern 2

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number : ");
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
 
-
-
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//
-//                 if (i == n - 1 || j == 0 || i == j || i > j) {
-//                     System.out.print("X");
-//                 } else {
-//                     System.out.print(" ");
-//                 }
-//             }
-//             System.out.println();
-//         }
-
-
-
-
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("X");
+            for (int j = 1; j <= n; j++) {
+                if (j >= i) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
 
-
- /*
- Method 3
- ex.
- System.out.println("A".repeat(5));  // ผลลัพธ์: AAAAA
- System.out.println("Hi".repeat(3)); // ผลลัพธ์: HiHiHi
-
- */
-//         for (int i = 1; i <= n; i++) {
-//             System.out.println("X".repeat(i));
-//         }
-
-
         sc.close();
     }
 }
+
+// Enter number: 5
+// XXXXX
+//  XXXX
+//   XXX
+//    XX
+//     X
